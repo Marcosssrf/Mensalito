baa497a5dd480fb232fada91c011004cc5b47bd5
Marcos <marcosssrf@hotmail.com>
2026-04-26 22:21:21 -0300

    Initialize Spring Boot backend project and IDE configuration

---
+++ backend/src/test/java/com/mensalito/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mensalito.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
